1e5d6ed26c2f2f16f7c10616c148a20a56d9366f
Melisa Tanrıkulu <melisatanrikulu07@gmail.com>
2025-12-11 01:43:52 +0300

    Manage Author functionality added

--- app/app.py
+++ app/app.py
@@ -7,6 +7,7 @@ from routes.auth import auth_bp
 from routes.main import main_bp
 from routes.admin import admin_bp
 from routes.profile import profile_bp  # <--- NEW IMPORT
+from routes.librarian import librarian_bp
 
 def create_app():
     app = Flask(__name__)
@@ -36,6 +37,7 @@ def create_app():
     app.register_blueprint(main_bp)
     app.register_blueprint(admin_bp)
     app.register_blueprint(profile_bp)  # <--- NEW REGISTRATION
+    app.register_blueprint(librarian_bp)
 
     return app
 

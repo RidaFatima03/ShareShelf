824121b2e11eee9ba698d3e2ea5f52f7bd680059
Melisa Tanrıkulu <melisatanrikulu07@gmail.com>
2025-12-21 18:38:50 +0300

    Copy management page

--- app/app.py
+++ app/app.py
@@ -8,6 +8,7 @@ from routes.main import main_bp
 from routes.admin import admin_bp
 from routes.profile import profile_bp  # <--- NEW IMPORT
 from routes.librarian import librarian_bp
+from routes.copies import copies_bp
 from routes.policy import policy_bp
 from routes.request import request_bp
 
@@ -40,6 +41,7 @@ def create_app():
     app.register_blueprint(admin_bp)
     app.register_blueprint(profile_bp)  # <--- NEW REGISTRATION
     app.register_blueprint(librarian_bp)
+    app.register_blueprint(copies_bp)
     app.register_blueprint(policy_bp)
     app.register_blueprint(request_bp)
 

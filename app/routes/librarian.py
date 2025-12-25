dea3d0abb6fc961b06180cf6b3172a48cb753d59
Melisa Tanrıkulu <melisatanrikulu07@gmail.com>
2025-12-25 21:22:47 +0300

    UI fixes

--- app/routes/librarian.py
+++ app/routes/librarian.py
@@ -165,8 +165,7 @@ def requests():
         JOIN Material m ON r.material_id = m.material_id
         WHERE r.request_type = 'Donation'{where_sql}
     """, params)
-    donations = cursor.fetchall()
-    donations.sort(key=lambda x: x["request_date"], reverse=True)
+    donations = sorted(cursor.fetchall(), key=lambda x: x["request_date"], reverse=True)
 
     # BOOK REQUESTS (Material)
     where_sql, params = build_request_filters("m.title", "m.author", search_values["book_requests"], active_tab == "book_requests")
